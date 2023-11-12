e8cc479a2d5745b90d2de532e937934c968eaecc
derryev <derryev@ssfs.org>
2023-11-12 18:18:37 -0500

    Completed and checked 6.4 homework intro to lists.

--- intro_lists.py
+++ intro_lists.py
@@ -4,7 +4,13 @@ def swap(list_one):
     :param list_one: a list of at least two elements
     :return: the same list with the first and last elements swapped
     """
-    pass # make sure to remove this line before beginning work on this function
+    num_elements = len(list_one)
+    place_last_variable = num_elements-1
+    og_last_variable = list_one.pop(place_last_variable)
+    og_first_variable = list_one[0]
+    list_one[0] = og_last_variable
+    list_one.append(og_first_variable)
+    return list_one
 
 
 def rotate_left(list_one):
@@ -13,7 +19,9 @@ def rotate_left(list_one):
     :param list_one: A list consisting of exactly three integers
     :return: a list where all the elements have been shifted 1 place to the left
     """
-    pass # make sure to remove this line before beginning work on this function
+    first_element = list_one.pop(0)
+    list_one.append(first_element)
+    return list_one
 
 
 def max_end(list_one):
@@ -23,4 +31,24 @@ def max_end(list_one):
     :param list_one: A list consisting of three elements - all integers
     :return: A list where all the elements are the larger of the first or last element of the original list
     """
-    pass # make sure to remove this line before beginning work on this function
+    first_element = list_one[0]
+    last_element = list_one[2]
+    if list_one[0] > list_one[2]:
+        list_one = [first_element, first_element, first_element]
+    elif list_one[2] > list_one[0]:
+        list_one = [last_element, last_element, last_element]
+    return list_one
+
+def main():
+    list_one = [1,2,3]
+    print(list_one)
+    print(swap(list_one))
+    list_one = [1,2,3]
+    print(list_one)
+    print(rotate_left(list_one))
+    list_one = [1, 2, 3]
+    print(list_one)
+    print(max_end(list_one))
+
+
+main()
