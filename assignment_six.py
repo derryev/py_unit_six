58327307bbecbc0fca2926950556918bcc0dd176
derryev <derryev@ssfs.org>
2023-11-29 09:55:49 -0500

    Drafting assignment 6 (V3)

--- assignment_six.py
+++ assignment_six.py
@@ -20,7 +20,7 @@ def is_duplicate(birthday_list):
 
 def main():
     while True:
-        times_run_sim = input(print("How many times would you like to simulate the birthday problem?"))
+        times_run_sim = input(print("How many times would you like to simulate the birthday problem? "))
         if times_run_sim.isdigit() is False or times_run_sim.isdecimal() is False:
             print("Please enter a positive, whole number in digits.")
         else:
@@ -29,15 +29,20 @@ def main():
     duplicate_counter = 0
     for x in range(int(times_run_sim)):
         birthday_list = generate_birthdays()
-        #print(birthday_list)
+        # print(birthday_list)
         if is_duplicate(birthday_list) is True:
             print("true")
             duplicate_counter = duplicate_counter+1
         elif is_duplicate(birthday_list) is False:
             print("False")
     print(duplicate_counter)
-    proportion_duplicate_lists = duplicate_counter/int(times_run_sim)
-    print("There were two of the same birthday")
+    if int(times_run_sim) == 1:
+        print("Out of " + times_run_sim + " simulated class, " + str(duplicate_counter) + " had duplicate birthdays.")
+    elif times_run_sim != 1:
+        print("Out of " + times_run_sim + " simulated classes, " + str(duplicate_counter) + " had duplicate birthdays.")
+    proportion_duplicate_lists = (duplicate_counter / int(times_run_sim))*100
+    print("This means there were duplicate birthdays about "+str(round(proportion_duplicate_lists, 4)) +
+          " percent of the time.")
 
 
 main()
