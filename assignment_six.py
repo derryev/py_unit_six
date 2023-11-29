1f5bc2c1ab8624ffe2aee26ecdd184cf2cda3f41
derryev <derryev@ssfs.org>
2023-11-29 08:56:55 -0500

    Drafting assignment 6 (V2)

--- assignment_six.py
+++ assignment_six.py
@@ -1,11 +1,14 @@
 
 import random
+
+
 def generate_birthdays():
     birthday_list = []
     for x in range(23):
         birthday = random.randint(1, 365)
         birthday_list.append(birthday)
     return birthday_list
+
 
 def is_duplicate(birthday_list):
     for birthday in birthday_list:
@@ -15,13 +18,26 @@ def is_duplicate(birthday_list):
     return False
 
 
-
 def main():
-    birthday_list = generate_birthdays()
-    print(birthday_list)
-    if is_duplicate(birthday_list) is True:
-        print("true")
-    else:
-        print("False")
+    while True:
+        times_run_sim = input(print("How many times would you like to simulate the birthday problem?"))
+        if times_run_sim.isdigit() is False or times_run_sim.isdecimal() is False:
+            print("Please enter a positive, whole number in digits.")
+        else:
+            break
+    print(times_run_sim)
+    duplicate_counter = 0
+    for x in range(int(times_run_sim)):
+        birthday_list = generate_birthdays()
+        #print(birthday_list)
+        if is_duplicate(birthday_list) is True:
+            print("true")
+            duplicate_counter = duplicate_counter+1
+        elif is_duplicate(birthday_list) is False:
+            print("False")
+    print(duplicate_counter)
+    proportion_duplicate_lists = duplicate_counter/int(times_run_sim)
+    print("There were two of the same birthday")
+
 
 main()
